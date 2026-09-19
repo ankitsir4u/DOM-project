881fce53820440473c62a9a72adf24c9c20d6778
ankitsir4u <ankitsir4u@gmail.com>
2026-09-19 15:33:55 +0530

    projects

--- 01-colorChanger/02_BMIcalculator/chaiaurcode.js
+++ 01-colorChanger/02_BMIcalculator/chaiaurcode.js
@@ -7,12 +7,6 @@ button.addEventListener("click", function (e) {
 
     e.preventDefault();
 
-    if (height=== '' || height < 0 || isNaN(height) ){
-        results.innerHTML = `please give a valid height ${height}`;
-    }else if (weight === '' || weight < 0 || isNaN(weight)) {
-        results.innerHTML = `please give a valid weight ${weight}`
-    }
-
     const heightValue = parseFloat(height.value);
     const weightValue = parseFloat(weight.value);
 
